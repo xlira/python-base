f8ac1fcbb10978cd7a2d828fc9bb6fe89a54c224
xlira <lucas.limasv@outlook.com.br>
2025-02-25 10:44:07 -0300

    script feito na primeira aula, explicação do SHEBANG

---
+++ dia-01/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python ----> Isso é um shebang, algo padrão para sistema UNIX que indica qual versão ou local o terminal que deve rodar o script esta. Para achar é só rodar o which python.
+#!/usr/bin/env python ----> Nesse caso você importa as variaveis de ambiente para o interpretador python.
+
+
+print("Hello, World!")
